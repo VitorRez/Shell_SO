44c978d438280996d34ed2a06837c05e61e3ec67
VitorRez <vitorez2002@gmail.com>
2023-04-30 11:27:32 -0300

    tentando implementar multiplos pipes

--- shellso.c
+++ shellso.c
@@ -9,13 +9,15 @@
 #include "argv.h"
 #include "auxiliar.h"
 
-void executa_arquivo(char* comando, char** argv){
+void executa_arquivo(char* comando, char** argv, int background){
     pid_t pid = fork();
     if(pid==0){
         execvp(comando, argv);
         exit(127);
     }else{
-        waitpid(pid,0,0);
+        if(background == 0){
+            waitpid(pid,0,0);
+        }
     }
 }
 
@@ -50,24 +52,51 @@ int return_in(int saved_stdin){
     close(saved_stdin);
 }
 
-int Pipe(char* process_l, char* process_r, char** argv_l, char ** argv_r, int out){
+void pipeline(char** command, int posicao_i, int posicao_f, int out, int background){
+    int first = 0;
+    int last = 0;
+    int in = 0;
+    int out = 0;
+    /*for(int i = 0; i < size; i++){
+        if(strcmp(command[i], "|") == 0){
+            if(first == 0){
+                first = i;
+            }
+            last = i;
+        }
+    }*/
+    for(int i = posicao_i; i <= posicao_f; i++){
+        for(int k = i; i < )
+    }
+}
+
+int Pipe(char* process_l, char* process_r, char** argv_l, char ** argv_r, int out, int in, int background){
     int fd[2];
     int saved_stdout;
 
-    if(pipe(fd) == -1){
+    /*if(pipe(fd) == -1){
         return 1;
-    }
+    }*/
 
     int pid1 = fork();
-    if(pid1 < 0){
+    /*if(pid1 < 0){
         return 2;
-    }
+    }*/
 
     if(pid1 == 0){
         dup2(fd[1], 1);
         close(fd[0]);
         close(fd[1]);
+        if(in != 0){
+            int saved_stdin = dup(0);
+            dup2(in, 0);
+            close(out);
+        }
         execvp(process_l, argv_l);
+        if(int != 0){
+            dup2(saved_stdin, 0);
+            close(saved_stdout);
+        }
     }
 
     int pid2 = fork();
@@ -94,9 +123,11 @@ int Pipe(char* process_l, char* process_r, char** argv_l, char ** argv_r, int ou
     close(fd[0]);
     close(fd[1]);
 
-    wait(NULL);
-    wait(NULL);
-    return 0;
+    if(background == 0){
+        wait(NULL);
+        wait(NULL);
+    }
+    return out;
 }
 
 //
@@ -113,6 +144,10 @@ void caminhar_nos_comandos(char* command_line){
     int saved_stdin = 0;
     int saved_stdout = 1;
     int out = 0;
+    int background = 0;
+    if(strcmp(command_parsed[num_linhas-1], "&") == 0){
+        background = 1;
+    }
     for(int i = 0; i < num_linhas; i++){
         if(strcmp(command_parsed[i], "<=") == 0){
             posicao_e = i;
@@ -141,13 +176,13 @@ void caminhar_nos_comandos(char* command_line){
         preenche_argv(argv_l, command_parsed, posicao_e, 0);
         char** argv_r = cria_argv(posicao_s-posicao_p);
         preenche_argv(argv_r, command_parsed, (posicao_s-posicao_p - 1), posicao_p+1);
-        int z = Pipe(argv_l[0], argv_r[0], argv_l, argv_r, out);
+        int z = Pipe(argv_l[0], argv_r[0], argv_l, argv_r, out, background);
         //delete_argv(argv_l, posicao_e);
         //delete_argv(argv_r, (posicao_s-posicao_p - 1));
     }else{
         char** argv = cria_argv(posicao_e);
         preenche_argv(argv, command_parsed, posicao_e, 0);
-        executa_arquivo(command_parsed[0], argv);
+        executa_arquivo(command_parsed[0], argv, background);
     }
     if(e == 1){
         return_in(saved_stdin);
